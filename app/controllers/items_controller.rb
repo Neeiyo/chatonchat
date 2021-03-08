308abd4d253c2ade63c607f8a76938632ff8db94
sassok <kevin.sassoo@gmail.com>
2021-03-08 15:10:31 +0100

    Index v1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,13 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @items = Item.new
+  end 
+
+  def create
+    @items = Item.create
+  end 
 end
