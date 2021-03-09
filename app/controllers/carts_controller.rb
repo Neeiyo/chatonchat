dc720933e0798437032fa1ccfdac4b869c016038
volpito <mpj.rigot@gmail.com>
2021-03-09 19:55:23 +0100

    boucle show

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,11 +3,12 @@ class CartsController < ApplicationController
 
 
   def show
+    @item = Item.all
     @cart = Cart.find_by(user_id: params[:id])
     if current_user.id == @cart.id
     else 
       redirect_to cart_path(current_user.id)
+    end
   end
-end
 
 end
