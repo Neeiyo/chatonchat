f5fa9f3a22b79a6a07266af549b87cf2a3bc76ba
anthopayan <anthony.payan08@outlook.fr>
2021-03-09 15:40:02 +0100

    show_carts

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,16 +1,17 @@
 class CartsController < ApplicationController
 
-  before_action :authenticate_user!, :user
+
 
   def show
-    @user = User.find(params['id'])
+    if current_user
+    @cart = Cart.find(params[:id])
+    else
+      redirect_to root_path
+    end
   end
 
   def create
-    if current_user == @user.id
-      @cart = Cart.create(user_id: current_user.id)
-    else
-      redirect_to root_path
-      flash[:danger]= "Bien essayé petit malin ! ;P"
-  end
+ 
+    
+end
 end
